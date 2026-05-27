600100e6ab0d72d12a8ace8d4e29f65e472387ff
TechPriestStefanus <stefanruhland7@gmail.com>
2026-05-27 14:32:11 +0200

    forth commit: added ability to call functions argument specified by ai agent and generate responses in a loop based on called functions and responses

---
+++ README.md
@@ -0,0 +1,6 @@
+AiAgent Guided Project 
+based on google genai
+
+key skillgaps made up for by Guide: Handling of genai objects/library syntax and required fields (ARRAY(items type))
+
+uv run main.py "(enter command for Agent)"
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import argparse
+import sys
 from dotenv import load_dotenv
 from google.genai import types
 from google import genai
@@ -8,9 +9,13 @@ from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.run_python_file import schema_run_python
 from functions.write_file import schema_write_file
+from functions.call_function import call_function
 from prompts import system_prompt
 
 available_functions = types.Tool(function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python, schema_write_file])
+
+# CONFIG
+loop_range = 10
 
 def main():
 
@@ -31,35 +36,70 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    #updated response to messages
-    #response = client.models.generate_content(model= "gemini-2.5-flash", contents= args+"In one paragraph.")
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]   
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature = 0), )
-                                              
-    user_prompt = (args.user_prompt)
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    called_functions = response.function_calls
-
-    if response.usage_metadata is None:
-        raise RuntimeError("API request failed")
-    elif args.verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
-        if called_functions:
-            for f_object in called_functions:
-                print(f"Calling function: {f_object.name}({f_object.args})")
-        else:    
-            print(response.text)
-    else:
-        if called_functions:
-            for f_object in called_functions:
-                print(f"Calling function: {f_object.name}({f_object.args})")
-        else:
-            print(response.text)
-
     
+
+    for _ in range(loop_range):
+        #updated response to messages
+        #response = client.models.generate_content(model= "gemini-2.5-flash", contents= args+"In one paragraph.")
+        messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]   
+        response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature = 0), )
+                                                
+        user_prompt = (args.user_prompt)
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        called_functions = response.function_calls #list of types.FunctionCall objects
+        results_list = []
+        
+        if response.candidates:  
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+
+        def function_loop():
+            for f_object in called_functions:
+                    function_call_result = call_function(f_object, args.verbose) #takes functioncallobject and verbose bool 
+                    if not isinstance(function_call_result.parts, list) or not function_call_result.parts:
+                        raise Exception("Problem with function result return object")
+                    
+                    elif not function_call_result.parts[0].function_response:
+                        raise Exception("Function Response Error 1")
+                    
+                    elif not function_call_result.parts[0].function_response.response:
+                        raise Exception("Function Response Error 2")
+                    
+                    else:
+                        
+                        results_list.append(function_call_result.parts[0])
+                        if args.verbose:
+                            print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        if response.usage_metadata is None:
+            raise RuntimeError("API request failed")
+        elif args.verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
+            if called_functions:
+                function_loop()
+                
+                    
+
+
+            else:   
+                print(response.text)
+        else:
+            if called_functions:
+                function_loop()
+            else:
+                print(response.text)
+        
+        messages.append(types.Content(role="user", parts=results_list))
+        if not called_functions:
+            break
+        if _ == (loop_range-1):
+            sys.exit(_ExitCode = 1)
+
+        
 
 if __name__ == "__main__":
     main()
